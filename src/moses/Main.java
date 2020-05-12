09f7b2dda87182b91958adf240ddaaabb52c4963
Jeremy D. Jones <j.jones1232@gmail.com>
2020-05-12 16:54:35 -0600

    Say hello to Chase

--- src/moses/Main.java
+++ src/moses/Main.java
@@ -3,6 +3,8 @@ package moses;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Hello, my name is Chase Moses");
+        System.out.println("Hello, my name is Chase Moses");
+        // Jeremy's Portion
+        System.out.println("Hello Chase Moses, my name is Jeremy");
     }
 }
